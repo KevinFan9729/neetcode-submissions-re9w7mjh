8235984c60dac90461b183bbbfe1ddbff3e2c196
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-04-21 19:15:01 -0400

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # feel like this is a simulation problem
+        # we can use a stack?
+        # we push the item in the stack
+            # check the top and the current
+        # Time O(n)
+        # Space O(n)
+        stack = [0] # pad a zero weight asteroid
+        for item in asteroids:
+            itemSurvived = False # this flag will take account say the asteroid survived and no more collisions 
+            top = stack[-1]
+            # willCollide
+                # top moves toward right, item moves towards left
+            willCollide = top >0 and item <0
+            if not willCollide: #  if not colliding
+                stack.append(item)
+            else:
+                while willCollide:
+                    if abs(top) == abs(item):
+                        itemSurvived = False # item did not survive
+                        stack.pop()
+                        break #  chain reaction not possible
+                    elif abs(top) < abs(item):
+                        stack.pop() #  old top is destoryed
+                        top = stack[-1]
+                        itemSurvived = True
+                    else: # the else case is top is bigger, item is destroyed and top remains
+                        itemSurvived = False # item did not survive 
+                        break
+                    willCollide = top >0 and item <0
+                if itemSurvived:
+                    stack.append(item)
+
+        return stack[1:]

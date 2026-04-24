240cbe8d11bc2abda6921ecb52d8c2fe878a59a1
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-04-24 14:13:04 -0400

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        # at each step we need to choose number into our result
+        # I notice that the start of the permutation needs to be unique
+        # we should track used item with indices now
+        # Time O(n!*n)
+        # space O(n)
+        res = []
+        used = set()
+        nums.sort()
+        def gen(curr):
+            if len(curr) == len(nums):
+                res.append(curr[:])
+                return
+            for i in range(len(nums)):
+                if i in used:
+                    continue # index was used before
+                nextIdx = i +1
+                currIdx = i
+                if nextIdx <= len(nums)-1:
+                    if nums[nextIdx] == nums[currIdx]:
+                        if nextIdx in used:
+                            continue
+                curr.append(nums[i])
+                used.add(i)
+                gen(curr)
+                curr.pop()
+                used.remove(i)
+            return
+        gen([])
+        return res

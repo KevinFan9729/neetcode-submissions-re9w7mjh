c55b73c2903cde5b3d23a9f089b96faaecef1238
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-04-24 14:14:12 -0400

    Update: permutations-ii - submission-0

--- Data Structures & Algorithms/permutations-ii/submission-0.py
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -12,14 +12,14 @@ class Solution:
             if len(curr) == len(nums):
                 res.append(curr[:])
                 return
-            for i in range(len(nums)-1, -1, -1):
+            for i in range(len(nums)):
                 if i in used:
                     continue # index was used before
-                prevIdx = i -1
+                nextIdx = i +1
                 currIdx = i
-                if prevIdx != -1:
-                    if nums[prevIdx] == nums[currIdx]:
-                        if prevIdx in used:
+                if nextIdx <= len(nums)-1:
+                    if nums[nextIdx] == nums[currIdx]:
+                        if nextIdx in used:
                             continue
                 curr.append(nums[i])
                 used.add(i)

2797a28ed293ffedf1c5187ce83c4bc30829489d
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-05-01 18:38:55 -0400

    Add: maximum-subarray - submission-11

---
+++ Data Structures & Algorithms/maximum-subarray/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadane 
+        # discard neg sum as they will not contribute
+        # Time O(n)
+        # Space O(1)
+        
+        currSum = 0
+        maxSum = nums[0]
+
+        for num in nums:
+            currSum += num
+            maxSum = max(maxSum, currSum)
+            currSum = max(currSum, 0)
+        return maxSum

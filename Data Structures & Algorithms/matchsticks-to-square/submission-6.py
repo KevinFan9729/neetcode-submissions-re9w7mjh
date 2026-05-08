8dd44292cda3368a5fd58a1e7ad05e53c9a8ea48
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-05-07 20:29:11 -0400

    Add: matchsticks-to-square - submission-6

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-6.py
@@ -0,0 +1,43 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        # we need to have four equal sides
+        # at the start, we have [0,0,0,0]
+        # at every step, at index i, what side should i goes to?
+        # Time O(4^n) # each steps have 4 decision
+        # Space O(n)
+        total = sum(matchsticks)
+        if total % 4 !=0:
+            return False
+        length = total // 4 # required size for a side
+        if max(matchsticks) > length:
+            return False # we cannot break sticks
+        sides = [0,0,0,0]
+        
+        def square(i):
+            # at index i, what side should i goes to?
+            count = 0
+            for side in sides:
+                if side == length:
+                    count +=1
+            if count == 4:
+                return True
+            # prune, for the current stack, do not try placing stick to already seen equivalent side
+            # say we have [0,0,0,0] and stick is 1, placing the 1 to location 0 is enough
+            seen = set() 
+            for pos in range(4):
+                stick = matchsticks[i]
+                if sides[pos] in seen:
+                    continue
+                seen.add(sides[pos])
+                if sides[pos]+ stick > length:
+                    continue # this stick cannot fit in this pos
+                sides[pos]+=stick
+                canForm = square(i+1)
+                if canForm:
+                    return True
+                sides[pos]-=stick #path failed, backtrack
+
+            return False
+
+        res = square(0)
+        return res 

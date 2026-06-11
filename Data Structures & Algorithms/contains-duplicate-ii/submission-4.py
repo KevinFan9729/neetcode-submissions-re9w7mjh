d160173724d96e839725255c8ce4ade3400346f6
Kevin Fan <60720175+KevinFan9729@users.noreply.github.com>
2026-06-11 19:39:11 -0400

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # the question is really asking
+        # can we find a window of size k+1 at most (can be smaller or equal)
+        # do we have any duplicate?
+        # sliding window of fixed size
+        # Time O(n)
+        # Space O(n)
+        wind = set()
+        left = 0       
+        for right in range(len(nums)):
+            if right - left > k:
+                wind.remove(nums[left])
+                left += 1
+            if nums[right] in wind:
+                return True 
+            wind.add(nums[right])
+        return False 
